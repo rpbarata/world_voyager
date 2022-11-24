f6d0b023ee9f393a9bdb1bd5e19c95dd63ce90b7
Rui Barata <ruipbarata@gmail.com>
2022-11-24 20:56:48 +0000

    Initial commit

---
+++ lib/world_voyager.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "world_voyager/version"
+
+module WorldVoyager
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/world_voyager_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe WorldVoyager do
+  it "has a version number" do
+    expect(WorldVoyager::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
